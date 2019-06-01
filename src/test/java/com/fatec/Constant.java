4d3ed82801b95535c8f1f704bbb0f322d64865c5
Laboratorio101 <Laboratorio101@LAB101-11>
2019-06-01 10:52:52 -0300

    "Novo Caso de Teste"

---
+++ src/test/java/com/fatec/Constant.java
@@ -0,0 +1,10 @@
+package com.fatec;
+
+public class Constant {
+
+	public static final String Path_TestData = "C:\\TEMP\\";
+	public static final String File_TestData = "cep_endereco.xlsx";
+	public static final String Chrome = "WebContent/WEB-INF/lib/chromedriver.exe";
+	public static final String Firefox = "WebContent/WEB-INF/lib/geckodriver.exe";
+	
+}
